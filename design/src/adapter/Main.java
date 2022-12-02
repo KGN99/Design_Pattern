a09ca66ad677e258b05e4bce6910a7d77545d344
kgn99 <y3ef9484@gmail.com>
2022-12-02 17:38:42 +0900

    어댑터 패턴

---
+++ design/src/adapter/Main.java
@@ -0,0 +1,21 @@
+package adapter;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
+
+        Cleaner cleaner = new Cleaner();
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        connect(adapter);
+
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110V adapter2 = new SocketAdapter(airConditioner);
+        connect(adapter2);
+
+    }
+    public static void connect(Electronic110V electronic110V){
+        electronic110V.powerOn();
+    }
+}
