30dde44af00fd2bb1bfc4a7c0484c450281b3c6d
kgn99 <y3ef9484@gmail.com>
2022-12-02 17:26:07 +0900

    싱글톤 패턴

--- design/src/Main.java
+++ design/src/Main.java
@@ -1,5 +1,8 @@
+import singleton.AClazz;
+import singleton.BClazz;
+import singleton.SocketClient;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    public static void main(String[] args) {}
+
 }
