7ec4c4e21c6f37a942d08bf90f9b0ded1872b6fc
kgn99 <y3ef9484@gmail.com>
2022-12-02 22:11:02 +0900

    옵저버 패턴

--- design/src/decorator/A3.java
+++ design/src/decorator/A3.java
@@ -2,6 +2,7 @@ package decorator;
 
 public class A3 extends AudiDecorator{
     public A3(ICar audi, String modeName) {
+
         super(audi, modeName, 1000);
     }
 }
--- design/src/decorator/Main.java
+++ design/src/decorator/Main.java
@@ -5,10 +5,10 @@ public class Main {
         ICar audi = new Audi(1000);
         audi.showPrice();
 
-        ICar audi3 = new A3(audi,"A3");
+        A3 audi3 = new A3(audi,"A3");
         audi3.showPrice();
 
-        ICar audi4 = new A4(audi,"A4");
+        A4 audi4 = new A4(audi,"A4");
         audi4.showPrice();
 
         ICar audi5 = new A5(audi,"A5");
